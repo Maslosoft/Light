f733b41c242306adbc27141c8d9ed22bca9476e9
Peter Maselkowski <peter@maslosoft.com>
2024-10-04 22:48:13 +0200

    Fallback implementation of context using static variables

---
+++ src/Contexts/Steady.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Malosoft\Contexts;
+
+class Steady
+{
+  private static $registry = [];
+  
+  public static function has($key)
+  {
+    return array_key_exists($registry);
+  }
+
+  public static function get($key)
+  {
+    return self::$registry[$key];
+  }
+
+  public static function put($key, $value)
+  {
+    self::registry[$key] = $value;
+  }
+}    
